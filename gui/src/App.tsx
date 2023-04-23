371d549c249d249b3f001eaaafef25b40398802b
sho037 <103867061+sho037@users.noreply.github.com>
2023-04-23 12:39:30 +0900

    💄 Inputエリアを作成

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import Header from './Components/Header';
+import React from "react";
+import Header from "./Components/Header";
+import { Box, Typography, Grid, TextField,Button } from "@mui/material";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Box sx={{ p: 2 }}>
+        <Typography variant="h5">あなたの最短時間を調べよう</Typography>
+        <hr />
+        <Grid
+          container
+          alignItems="center"
+          justifyContent="center"
+          direction="column"
+        >
+          {/* @TODO 文字色変更 */}
+          <Typography variant="h6" color="">
+            ＊現在は京阪電車のみに対応しています。
+          </Typography>
+          <TextField id="peparture-point" label="出発地" variant="outlined" />
+          <br />
+          <TextField id="destination-point" label="目的地" variant="outlined" />
+
+          <br />
+          <Button variant="contained">検索</Button>
+        </Grid>
+      </Box>
     </>
   );
-}
+};
 
 export default App;
